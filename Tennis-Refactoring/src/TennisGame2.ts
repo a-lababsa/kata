848c06f327a2e502656a2fe507586211f41d9df7
Alexandre lababsa <alexandre@MacBook-Pro-de-Alexandre.local>
2022-09-11 22:28:45 +0200

    feat: TennisGame2 cleanup

--- Tennis-Refactoring/src/TennisGame2.ts
+++ Tennis-Refactoring/src/TennisGame2.ts
@@ -1,12 +1,6 @@
 import { TennisGame, Player, Score, ScoreAll } from './TennisGame';
 
 export class TennisGame2 implements TennisGame {
-  P1point: number = 0;
-  P2point: number = 0;
-
-  P1res: string|null = '';
-  P2res: string|null = '';
-
   private player1: Player;
   private player2: Player;
 
@@ -78,6 +72,18 @@ export class TennisGame2 implements TennisGame {
     return ''
   }
 
+  advantage(player: Player):string {
+    const rival = this.getRival(player)
+    const advantage = rival && player.score > rival?.score && rival?.score >= 3
+    return advantage ? `Advantage ${player.name}` : ''
+  }
+
+  win(player: Player):string {
+    const rival = this.getRival(player)
+    const win = rival && player.score >= 4 && rival?.score >= 0 && (player.score - rival?.score) >= 2
+    return win ? `Win for ${player.name}` : ''
+  }
+
   getScore(): string {
     let score: string = '';
     if (this.equality() && this.player1.score < 4) {
@@ -110,49 +116,32 @@ export class TennisGame2 implements TennisGame {
       score = secondMatch
     }
 
-    if (this.P1point > this.P2point && this.P2point >= 3) {
-      score = 'Advantage player1';
+    const firstAdvantage = this.advantage(this.player1)
+    if (firstAdvantage) {
+      score = firstAdvantage
     }
 
-    if (this.P2point > this.P1point && this.P1point >= 3) {
-      score = 'Advantage player2';
+    const secondAdvantage = this.advantage(this.player2)
+    if (secondAdvantage) {
+      score = secondAdvantage
     }
 
-    if (this.P1point >= 4 && this.P2point >= 0 && (this.P1point - this.P2point) >= 2) {
-      score = 'Win for player1';
+    const firstWin =  this.win(this.player1)
+    if (firstWin) {
+      score = firstWin
     }
-    if (this.P2point >= 4 && this.P1point >= 0 && (this.P2point - this.P1point) >= 2) {
-      score = 'Win for player2';
+
+    const secondWin =  this.win(this.player2)
+    if (secondWin) {
+      score = secondWin
     }
     return score;
   }
 
-  SetP1Score(score: number): void {
-    for (let i = 0; i < score; i++) {
-      this.P1Score();
-    }
-  }
-
-  SetP2Score(score: number): void {
-    for (let i = 0; i < score; i++) {
-      this.P2Score();
-    }
-  }
-
-  P1Score(): void {
-    this.player1.score++;
-    this.P1point++
-  }
-
-  P2Score(): void {
-    this.player2.score++
-    this.P2point++;
-  }
-
   wonPoint(player: string): void {
     if (player === 'player1')
-      this.P1Score();
+      this.player1.score++;
     else
-      this.P2Score();
+      this.player2.score++
   }
 }
